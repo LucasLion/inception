a88aa99e78a35e92de2bdcf0e6c2ea286c32994e
Lucas Lion <llion@k0r3p10.42mulhouse.fr>
2023-12-06 16:59:22 +0100

    save

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -4,35 +4,53 @@
 
 
 
-sleep 5
+sleep 10
 
-curl				-O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod				+x wp-cli.phar
-mv					-f wp-cli.phar /usr/local/bin/wp
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv -f wp-cli.phar /usr/local/bin/wp
 
-/usr/local/bin/wp	--info
-/usr/local/bin/wp	core download --allow-root --path="/var/www/html"
+/usr/local/bin/wp --info
+/usr/local/bin/wp core download --allow-root --path="/var/www/html"
 
-rm					-f /var/www/html/wp-config.php
-#cp					./wp-config.php /var/www/html/wp-config.php
-cp					./wp-config.php /var/www/wordpress/wp-config.php
+rm -f /var/www/html/wp-config.php
+cp ./wp-config.php /var/www/html/wp-config.php
+#cp ./wp-config.php /var/www/wordpress/wp-config.php
 
-/usr/local/bin/wp	core install \
+/usr/local/bin/wp core install \
 					--allow-root \
 					--path="/var/www/html" \
-					--url=${WORDPRESS_HOST} \
-					--title=${WORDPRESS_TITLE} \
-					--admin_user=${WORDPRESS_ADMIN_USER} \
-					--admin_password=${WORDPRESS_ADMIN_PASSWORD} \
-					--admin_email=${WORDPRESS_ADMIN_EMAIL} \
+					--url="llion.42.fr" \
+					--title="inception" \
+					--admin_user="godfather" \
+					--admin_password="godfather_passwd" \
+					--admin_email="godfather@42mulhouse.fr" \
 					--skip-email
 
-/usr/local/bin/wp	user create \
+/usr/local/bin/wp user create \
 					--allow-root \
 					--path="/var/www/html" \
-					${WORDPRESS_USER} \
-					${WORDPRESS_EMAIL} \
+					"llion" \
+					"llion@42mulhouse.fr" \
 					--role=author \
-					--user_pass=${WORDPRESS_PASSWORD}
+					--user_pass="llion_passwd"
 
-exec	php-fpm7 -F
+#/usr/local/bin/wp core install \
+#					--allow-root \
+#					--path="/var/www/html" \
+#					--url=${WORDPRESS_HOST} \
+#					--title=${WORDPRESS_TITLE} \
+#					--admin_user=${WORDPRESS_ADMIN_USER} \
+#					--admin_password=${WORDPRESS_ADMIN_PASSWORD} \
+#					--admin_email=${WORDPRESS_ADMIN_EMAIL} \
+#					--skip-email
+#
+#/usr/local/bin/wp user create \
+#					--allow-root \
+#					--path="/var/www/html" \
+#					${WORDPRESS_USER} \
+#					${WORDPRESS_EMAIL} \
+#					--role=author \
+#					--user_pass=${WORDPRESS_PASSWORD}
+
+exec php-fpm -F
