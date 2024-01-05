05e267f671c49e0eb09bce09afd68d4be8b3f098
Lucas Lion <llion@k0r3p10.42mulhouse.fr>
2024-01-05 15:21:45 +0100

    save

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -12,17 +12,12 @@ mv -f wp-cli.phar /usr/local/bin/wp
 
 if [ ! -e "wp-config.php" ]; then
 	wp config create	--allow-root \
-						--dbname="inception" \
-						--dbuser="inception" \
-						--dbpass="inception_passwd" \
-						--dbhost=mariadb:3306 --path='/var/www/wordpress'
+				--dbname=$MARIADB_DATABASE \
+				--dbuser=$MARIADB_USER \
+				--dbpass=$MARIADB_PASSWORD \
+				--dbhost=mariadb:3306 --path='/var/www/wordpress'
 fi
 
-#wp config create	--allow-root \
-#					--dbname=$MARIADB_DATABASE \
-#					--dbuser=$MARIADB_USER \
-#					--dbpass=$MARIADB_PASSWORD \
-#					--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 /usr/local/bin/wp --info
 /usr/local/bin/wp core download --allow-root --path="/var/www/html"
